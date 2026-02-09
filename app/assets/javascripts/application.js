ad6e472f0601038b6b8af736a9ea572803b96cba
Satnam Singh <satnam.singh@digital.hmrc.gov.uk>
2026-02-09 12:52:16 +0000

    Updating text changes for UR6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,8 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
+import HMRCFrontend from "hmrc-frontend"; document.addEventListener('DOMContentLoaded', () => { HMRCFrontend.initAll(); });
+
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
